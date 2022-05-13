eb93b6763745328b992bbb47522d404ae790047b
Marco Ramírez <marco.rs@wizeline.com>
2022-05-13 13:40:40 -0500

    Initial Commit

---
+++ buckverkauf/buckverkaufApp.swift
@@ -0,0 +1,17 @@
+//
+//  buckverkaufApp.swift
+//  buckverkauf
+//
+//  Created by Marco Ramírez Solís on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct buckverkaufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
